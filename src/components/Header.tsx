35ff245513f805648369dc23af76cec5a9de5423
Akhil Agrawal <126651810+swastik-akhil@users.noreply.github.com>
2024-07-30 15:16:28 +0530

    Changed header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Header: FC = () => {
   return (
-    <Link href="/" className="block mx-auto p-4 bg-orange-200 relative">
+    <Link href="/" className="block mx-auto p-4 bg-teal-300 relative">
       <div className="text-2xl font-bold text-center">
         Next.js Bookstore
       </div>
